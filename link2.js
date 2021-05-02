f84bcbbef29158dd531c454ddc7ff67bb09e3779
sangjun0082 <83547688+sangjun0082@users.noreply.github.com>
2021-05-03 01:00:57 +0900

    V1.0

---
+++ link2.js
@@ -0,0 +1,36 @@
+function goLink(url)
+{
+	if(url=="telegram"){
+		window.open('https://t.me/mgcenter77','_blank');
+	}else if(url=="theking"){
+		location.href="index.html";
+	}else if(url=="merit"){
+		location.href="index.html";
+	}else if(url=="sands"){
+		location.href="index.html";
+	}else if(url=="coin"){
+		window.open('https://pqr92.com','_blank');
+	}else if(url=="first"){
+		window.open('https://fcb456.com','_blank');
+	}else if(url=="007"){
+		location.href="index.html";
+	}else if(url=="thezone"){
+		location.href="index.html";
+	}else if(url=="pharaoh"){
+		window.open('http://omx43.com','_blank');
+	}else if(url=="solaire"){
+		window.open('http://ppo908.com','_blank');
+	}else if(url=="sm"){
+		window.open('http://asf04.com','_blank');
+	}else if(url=="sol"){
+		window.open('http://ppo908.com','_blank');
+	}else if(url=="avengers"){
+		location.href="index.html";
+	}else if(url=="link1"){
+		location.href="index.html";
+	}else if(url=="link2"){
+		location.href="index.html";
+	}else if(url=="link3"){
+		location.href="index.html";
+	}
+}
